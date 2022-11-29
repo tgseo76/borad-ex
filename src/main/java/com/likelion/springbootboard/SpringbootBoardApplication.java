ad7c65d690e9d256a5a2cbfa55afbe344c33c55b
menuHwang <hmin0206@naver.com>
2022-11-30 00:18:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/likelion/springbootboard/SpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.springbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBoardApplication.class, args);
+    }
+
+}
